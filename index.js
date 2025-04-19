a347caeb1b626662319b1ebbc7f4c12170dca5d0
Ramsy007 <girdharmishra10102003@gmail.com>
2025-04-19 15:41:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Hello World Route
+app.get('/', (req, res) => {
+  res.send('Hello World from Node.js!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
